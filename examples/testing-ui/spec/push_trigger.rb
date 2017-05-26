d6fa3fab953ed7306315f0592beaaf3b4931ff81
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2017-05-26 11:35:05 -0300

    Refactoring of Ui integrated with-ops-alerts

---
+++ examples/testing-ui/spec/push_trigger.rb
@@ -0,0 +1,28 @@
+load './test_case.rb'
+
+describe "Pushing Triggers " do
+  # Starting the resquest
+  before(:each) do
+    @parameters = {"name" =>"Pushing Triggers", "hawkular-environment" => ENV['HAWKULAR_ENVIRONMENT'],
+      "hawkular-url" => "hawkular/alerts/import/all", "ssl"=> ENV['HAWKULAR_USE_SSL'].to_bool, "http-method"=> 'POST'}
+
+      @headers = {"hawkular-tenant" => ENV['HAWKULAR_TENANT'],
+          "content-type" => 'application/json',
+           "authorization" => ENV['HAWKULAR_TOKEN'],
+           "cache-control" => 'no-cache'}
+   end
+
+  it "HTTP Response should be 200 if body is not empty" do
+    @parameters["body"] = JSON.parse(File.read('autoresolve-trigger.json')).to_json
+    test_case = TestCase.new(@parameters, @headers)
+    response = test_case.peform_request
+    expect(response.code).to  eq("200")
+  end
+
+ it "HTTP Response should be 400 if body is empty" do
+    @parameters["body"] = ''
+    test_case = TestCase.new(@parameters, @headers)
+    response = test_case.peform_request
+    expect(response.code).to  eq("400")
+ end
+end
---
+++ examples/testing-ui/spec/test_case.rb
@@ -0,0 +1,50 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+class TestCase
+def initialize(parameters, headers)
+  @name = parameters["name"]
+  @url = URI(parameters["hawkular-environment"] + parameters["hawkular-url"])
+  @http = Net::HTTP.new(@url.host, @url.port)
+  @http.use_ssl = parameters["ssl"]
+  @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  case parameters["http-method"]
+
+  when 'GET'
+    @request = Net::HTTP::Get.new(@url)
+    configure_base_request(headers)
+
+  when 'POST'
+    @request = Net::HTTP::Post.new(@url)
+    configure_base_request(headers)
+    configure_body_request(parameters["body"])
+
+  when 'DELETE'
+    @request = Net::HTTP::Delete.new(@url)
+    configure_base_request(headers)
+
+  when 'PUT'
+    @request = Net::HTTP::Put.new(@url)
+    configure_base_request(headers)
+  end
+end
+
+def configure_base_request(headers)
+  headers.each do |key, value|
+    @request[key.to_s] = value
+  end
+end
+
+
+def configure_body_request(body)
+  @request.body=body
+end
+
+def peform_request
+  return @http.request(@request)
+end
+
+
+
+end
